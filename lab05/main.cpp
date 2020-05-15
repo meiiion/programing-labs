7f939c014fda9e6bd1089838eebd506394a598ec
meiiion <29644616+meiiion@users.noreply.github.com>
2020-05-16 02:45:33 +0300

    Update main.cpp

--- lab05/main.cpp
+++ lab05/main.cpp
@@ -1,47 +1,10 @@
-//Not Finished!!!!!!!!!
+// NOT FINISHED
+
 #include <iostream>
 #include <string>
+#include "line.h"
 #define BUFFER_LIMIT 150
 using namespace std;
-
-
-char* getarray(size_t *size) {
-	int c = 0;
-	*size = 0;
-	size_t buffer = 32;
-	char *line = (char *)(malloc(buffer));
-	if (line == NULL) return nullptr;
-	while ((c = getchar()) != '\n' && buffer <= BUFFER_LIMIT)
-	{
-		(*size)++;
-		if (*size <= buffer) line[*size - 1] = c;
-		else
-		{
-			char* ptr = (char*)realloc(line, buffer += 8);
-			if (ptr == nullptr)
-			{
-				free(line);
-				return nullptr;
-			}
-			line = ptr;
-			line[*size - 1] = c;
-		}
-	}
-	if (*size < buffer) line[*size] = '\0';
-	else
-	{
-		char* ptr = (char*)realloc(line, buffer += 1);
-		if (ptr == nullptr)
-		{
-			free(line);
-			return nullptr;
-		}
-		line = ptr;
-		line[*size] = '\0';
-	}
-	line[*size] = '\0';
-	return line;
-}
 
 int main() {
 	size_t size = 0;
