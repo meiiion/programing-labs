3ce01b8a142d1b07c3232668ab2200c6ba54e992
meiiion <29644616+meiiion@users.noreply.github.com>
2020-05-17 00:40:50 +0300

    Update main.cpp

--- lab05/main.cpp
+++ lab05/main.cpp
@@ -1,15 +1,29 @@
-// NOT FINISHED
-
 #include <iostream>
 #include <string>
 #include "line.h"
-#define BUFFER_LIMIT 150
 using namespace std;
 
 int main() {
-	size_t size = 0;
-	char* p = getarray(&size);
-	printf("%s, %d", p, size);
-	
+	setlocale(LC_ALL, "");
+	size_t first_size = 0, second_size = 0;
+	printf("Введите первое слово: ");
+	char *firstword = getarray(&first_size);
+	printf("Введите второе слово: ");
+	char *secondword = getarray(&second_size);
+	printf("Все возможные комбинации кроссворда:\n\n");
+	for (size_t i = 0, counter = 1; i<first_size; i++)
+	{
+		for (size_t j = 0; j<second_size; j++)
+		{
+			if (firstword[i] == secondword[j])
+			{
+				printf("%d)\n", counter++);
+				printwords(firstword, secondword, &i, &j);
+				putchar('\n');
+			}
+		}
+	}
+	free(firstword);
+	free(secondword);
 	return 0;
 }
