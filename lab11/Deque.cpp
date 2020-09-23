958678f13443b8ac457080311f28eecd653d47b6
meiiion <29644616+meiiion@users.noreply.github.com>
2020-09-23 15:43:34 +0600

    Update Deque.cpp

--- lab11/Deque.cpp
+++ lab11/Deque.cpp
@@ -1,16 +1,15 @@
 #include "Deque.h"
-#include <stdio.h>
+const int default_size = 3;
 
 Deque::Deque() {
-	queue = new int[16]();
+	size = default_size;
+	queue = new int[size]();
 	count = 0;
 }
 
-Deque::Deque(int queueSize) {
-	if (queueSize < 129)
-		queue = new int[queueSize]();
-	else
-		queue = new int[16]();
+Deque::Deque(int size) {
+	this->size = size;
+	queue = new int[size]();
 	count = 0;
 }
 
@@ -19,7 +18,7 @@ Deque::~Deque() {
 }
 
 int Deque::PushFront(int _num) {
-	if (count > 49)
+	if (count > size)
 		return -1;
 
 	queue[count++] = _num;
@@ -27,7 +26,7 @@ int Deque::PushFront(int _num) {
 }
 
 int Deque::PushBack(int _num) {
-	if (count > 49)
+	if (count > size)
 		return -1;
 	for (int i = count; i > 0; i--)
 		queue[i] = queue[i - 1];
@@ -47,7 +46,8 @@ int Deque::PopBack() {
 		return 0;
 	int c = queue[0];
 	for (int i = 0; i < count - 1; i++)
-		queue[i] = queue[i - 1];
+		queue[i] = queue[i + 1];
+	count--;
 	return c;
 }
 
